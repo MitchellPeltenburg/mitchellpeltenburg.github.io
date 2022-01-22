8694fdc1df81cb45dc4057a4c8e1f5474cdb4631
MitchellPeltenburg <mitchell.peltenburg@gmail.com>
2022-01-21 21:45:37 -0500

    updated ICE-2

--- Scripts/app.js
+++ Scripts/app.js
@@ -33,7 +33,29 @@
         console.log("Contact Page");
     }
 
-    // same function, however this method is better
+
+    // Step 1 - get a reference to an entry point(s) (insertion / deletion)
+    let MainContent = document.getElementsByTagName("main")[0];
+    let DocumentBody = document.body;
+
+    // Step 2 - Create a HTML Element in memory
+    let MainParagraph = document.createElement("p");
+    let Article = document.createElement("article");
+    let ArticleParagraph = '<p id="ArticleParagraph" class="mt-3">This is the article paragraph</p>'
+
+    // Step 3 - Configure new Element
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph`;
+    MainParagraph.textContent = SecondString;
+    Article.setAttribute("class", "container");
+
+    // Step 4 - perform insertion / deletion
+    MainContent.appendChild(MainParagraph);
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
     let Start = function()
     {
         console.log("App Started!");
