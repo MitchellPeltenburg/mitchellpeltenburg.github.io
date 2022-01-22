f7509f8f30cbf8b4eab90e2c631760425b78d081
MitchellPeltenburg <mitchell.peltenburg@gmail.com>
2022-01-21 22:30:34 -0500

    Updated ICE-2

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,23 @@
             location.href = "about.html";
             console.log("About Us Button Clicked!");
         });
+        
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion)
+        let MainContent = document.getElementsByTagName("main")[0];
+        //let DocumentBody = document.body;
+
+        // Step 2 - Create a HTML Element in memory
+        let MainParagraph = document.createElement("p");
+
+        // Step 3 - Configure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        //let FirstString = "This is";
+        //let SecondString = `${FirstString} the Main Paragraph`;
+        MainParagraph.textContent = "This is the Main Paragraph!";
+
+        // Step 4 - perform insertion / deletion
+        MainContent.appendChild(MainParagraph);
     }
 
     function DisplayProductsPage()
@@ -33,29 +50,7 @@
         console.log("Contact Page");
     }
 
-
-    // Step 1 - get a reference to an entry point(s) (insertion / deletion)
-    let MainContent = document.getElementsByTagName("main")[0];
-    let DocumentBody = document.body;
-
-    // Step 2 - Create a HTML Element in memory
-    let MainParagraph = document.createElement("p");
-    let Article = document.createElement("article");
-    let ArticleParagraph = '<p id="ArticleParagraph" class="mt-3">This is the article paragraph</p>'
-
-    // Step 3 - Configure new Element
-    MainParagraph.setAttribute("id", "MainParagraph");
-    MainParagraph.setAttribute("class", "mt-3");
-    let FirstString = "This is";
-    let SecondString = `${FirstString} the Main Paragraph`;
-    MainParagraph.textContent = SecondString;
-    Article.setAttribute("class", "container");
-
-    // Step 4 - perform insertion / deletion
-    MainContent.appendChild(MainParagraph);
-    Article.innerHTML = ArticleParagraph;
-    DocumentBody.appendChild(Article);
-
+    //named function
     let Start = function()
     {
         console.log("App Started!");
