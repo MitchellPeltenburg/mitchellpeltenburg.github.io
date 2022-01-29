9ac10a68c39edb31f8aee56a634d7787b2719bb2
MitchellPeltenburg <mitchell.peltenburg@gmail.com>
2022-01-28 23:50:18 -0500

    updated ice-3

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -31,6 +31,28 @@ class Contact
         this.m_emailAddress = emailAddress;
     }
 
+    //public methods
+    serialize()
+    {
+        if(this.FullName !== "" && this.ContactNumber !== "" && this.EmailAddress !== "")
+        {
+            return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+        }
+        else
+        {
+            console.error("One or more properties of the Contact are missing or empty");
+            return null;
+        }
+    }
+
+    deserialize(data)
+    {
+        let propertyArray = data.split(","); //assume that data is a comma-separated list of properties (strings)
+        this.FullName = propertyArray[0];
+        this.ContactNumber = propertyArray[1];
+        this.EmailAddress = propertyArray[2];
+    }
+
     // constructor
     constructor(fullName, contactNumber, emailAddress)
     {
